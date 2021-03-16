01b9b59b2cd27cfaf66ce1e9c1287a55ffa27313
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2021-03-16 21:01:17 +0800

    Update redirect page

--- src/components/redirected-page.js
+++ src/components/redirected-page.js
@@ -2,19 +2,24 @@ import React, { useEffect } from "react"
 import { navigate } from "gatsby"
 
 import "./redirected-page.scss"
+import generalSiteDetails from "../data/details.json";
 
-export default function RedirectedPage({ target }) {
+export default function RedirectedPage({ target, debug }) {
   console.log("Attempting to redirect to", target)
 
   useEffect(() => {
-    navigate(target)
+    if(!debug){
+      navigate(target)
+    }
   })
 
   // feel free to make this nicer
   return (
     <div className="redirect">
-      <a href={target}>
-        Redirecting you to {target} ... click here to proceed immediately.
+      <img class="redirect-logo" src={"/" + generalSiteDetails.currentYear + "/assets/img/redirect.webp"} />
+      <h2 class="redirect-text">Redirecting...</h2>
+      <a href={target} class="redirect-link">
+        Click here to proceed to the site immediately
       </a>
     </div>
   )
