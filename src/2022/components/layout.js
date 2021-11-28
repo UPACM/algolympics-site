5e92e36592bb17a18f70c810b8ed798b6b78fd7b
jolyuh <jrdizon4@up.edu.ph>
2021-11-29 00:19:38 +0800

    Initial commit with barebones landing page

---
+++ src/2022/components/layout.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Header from "../../2022/components/header.js"
+import Footer from "../../2022/components/footer.js"
+
+import "../../2022/styles/global.scss"
+
+export default function Layout({ isIndex, contentClass, contentId, children }) {
+  return (
+    <>
+      <Header isIndex={isIndex} />
+      <div className={"content wrapper " + contentClass} id={contentId}>
+        {children}
+      </div>
+      <Footer />
+    </>
+  )
+}
