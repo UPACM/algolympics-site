5102806914b31f8191549b98beba2213e03e8545
Julia Francesca Dizon <jrdizon4@up.edu.ph>
2022-02-01 12:17:23 +0800

    Revert "Add problems pages"
    This reverts commit c957078323c51decc9dfe27b2701eecbfe99b25b.

--- src/2022/components/header.js
+++ src/2022/components/header.js
@@ -138,13 +138,13 @@ export default function Header({ isIndex }) {
               >
                 Mechanics
               </Link>
-              <Link
+              {/* <Link
                 className="navlink"
                 activeClassName="navlink"
                 to="/2022/problems/"
               >
                 Problems
-              </Link>
+              </Link> */}
               <Link
                 className="navlink"
                 activeClassName="navlink"
