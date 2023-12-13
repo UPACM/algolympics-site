a9c6abcaed356786953b7b34b4c2d4c397ed08e8
nathaniel-mercado <nmercado0517@gmail.com>
2023-12-13 22:35:36 +0800

    Added 2024 base page

---
+++ src/2024/components/algolympics-meta.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+/*
+sitePage: page directory of the site, e.g. "/2023/Live"
+*/
+export default function AlgolympicsMeta({
+  algoYear,
+  pageName,
+  sitePage,
+  isIndex,
+}) {
+  var titleTag = (
+    <title>
+      {pageName} - Algolympics {algoYear}
+    </title>
+  )
+  if (isIndex) {
+    titleTag = <title>Algolympics {algoYear}</title>
+  }
+  return (
+    <Helmet>
+      <meta charSet="utf-8" />
+      {titleTag}
+      <link rel="canonical" href={"https://algolympics.upacm.net" + sitePage} />
+      <meta
+        name="Description"
+        content="The Algolympics is an annual intercollegiate programming competition open to all interested schools, colleges and universities in the Philippines, inspired by the likes of ICPC."
+      />
+    </Helmet>
+  )
+}
---
+++ src/pages/2024/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+    return(
+    <>
+    <h1>2024</h1>
+    <p>Hello</p>
+    </>
+    )
+}
